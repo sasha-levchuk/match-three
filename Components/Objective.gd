f0ebd70d671b0801aa8e2f5dd813e1786354cc20
anexlader <anexlader@gmail.com>
2026-03-30 00:13:00 +0300

    send block sprite on a curved path

--- Components/Objective.gd
+++ Components/Objective.gd
@@ -10,9 +10,8 @@ func _init(_block: Block):
 
 
 func collect():
-	block.z_index = 88
 	Game.test_signal.emit(block.sprite)
-	await block.get_tree().create_timer(2).timeout
+	#await block.get_tree().create_timer(2).timeout
 	#var tween := create_tween()
 	#tween.set_parallel(true)
 	#tween.tween_property(block.sprite, 'global_position', Vector2(170,64), 0.3).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_IN)
